fbf2d6dce097b0b0ca91147e736e4be80fee9ff9
ander <andersonbarreto2001@gmail.com>
2025-01-05 16:13:43 +0100

    typing data

--- hangman/display.py
+++ hangman/display.py
@@ -1,92 +1,15 @@
 class HangmanDisplay:
+    def __init__(self, hangman_states: list[str]) -> None:
+        self.hangman_states = hangman_states
 
-    def __init__(self):
-        self.HANGMAN_STATES = [
-            """=========""",
-            """
-           |
-           |
-           |
-           |
-           |
-    =========
-    """,
-            """
-      +----+
-           |
-           |
-           |
-           |
-           |
-   =========
-        """,
-            """
-      +----+
-      |    |
-           |
-           |
-           |
-           |
-   =========
-        """,
-            """
-      +----+
-      |    |
-      o    |
-           |
-           |
-           |
-   =========
-        """,
-            """
-      +----+
-      |    |
-      o    |
-      |    |
-           |
-           |
-   =========
-        """,
-            """
-  
-        """,
-            """
-      +----+
-      |    |
-      o    |
-     /|    |
-           |
-           |
-   =========
-        """,
-            """
-      +----+
-      |    |
-      o    |
-     /|\   |
-           |
-           |
-   =========
-        """,
-            """
-      +----+
-      |    |
-      o    |
-     /|\   |
-     /     |
-           |
-   =========
-        """,
-            """
-      +----+
-      |    |
-      o    |
-     /|\   |
-     / \   |
-           |
-   =========
-        """,
-        ]
+    def get_total_attempts(self) -> int:
+        return len(self.hangman_states)
 
-    def test_print(self, attemps_left):
-        print(self.HANGMAN_STATES[attemps_left])
+    def display_hangman(self, attempts_left: int) -> None:
+        print(self.hangman_states[attempts_left])
+
+    def display_presentation(self, word: str, attempts: int) -> None:
+        total_letters = len(word)
+        print("Welcome to the Hangman game!")
+        print("The word has", total_letters, "letters in total", "_ " * total_letters)
+        print("You have", attempts, "attempts. ¡Good Luck!")
--- hangman/words.py
+++ hangman/words.py
@@ -1,30 +1,30 @@
 from random import randrange
+from typing import List
 
 
 class WordManagerError(Exception):
-    def __init__(self, message):
-        Exception.__init__(self, message)
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
 
 
 class WordManager:
-    def __init__(self, words_list: list[str]):
+    def __init__(self, words_list: List[str]) -> None:
         if not self.is_string_list(words_list):
-            raise WordManagerError("The list not contains strings")
-        self.words_list = words_list
+            raise WordManagerError("The list does not contain strings")
+        self.words_list: List[str] = words_list
 
-    def get_random_word(self):
-        size = len(self.words_list)
+    def get_random_word(self) -> str:
+        size: int = len(self.words_list)
         if size != 0:
-            r = randrange(size)
-            random_word = self.words_list[r]
+            r: int = randrange(size)
+            random_word: str = self.words_list[r]
             return random_word
         else:
             raise WordManagerError("Error no word found")
 
     @staticmethod
-    def is_string_list(words: list[str]) -> bool:
-        is_valid = True
+    def is_string_list(words: List[str]) -> bool:
         for elem in words:
             if not isinstance(elem, str):
-                is_valid = False
-        return is_valid
+                return False
+        return True
